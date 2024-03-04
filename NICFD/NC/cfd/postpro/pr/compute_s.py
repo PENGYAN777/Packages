f9f4b1e3ae72f2be35e7bb578e147f156c656bf0
PENGYAN777 <peng.yan@mail.polimi.it>
2024-03-04 16:03:24 +0100

    NC cfd

---
+++ NICFD/NC/cfd/postpro/pr/compute_s.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar  4 10:51:54 2024
+
+@author: yan
+"""
+
+import numpy as np
+import CoolProp as CP
+import pandas as pd
+
+fluidname = "PR::MD4M"
+data = pd.read_csv("m4.csv", ",")
+P = data.iloc[:,6] 
+T = data.iloc[:,8] 
+D = data.iloc[:,0] 
+print("size", P.index)
+
+G = np.zeros(P.size)
+Z = np.zeros(P.size)
+# ht = np.zeros(P.size)
+s = np.zeros(P.size)
+# c = np.zeros(P.size)
+for i in P.index:
+    G[i] = CP.CoolProp.PropsSI('fundamental_derivative_of_gas_dynamics',
+                                'T',T[i],'P',P[i],fluidname)
+    Z[i] =  CP.CoolProp.PropsSI('Z','T',T[i],'P',P[i],fluidname)
+    s =  CP.CoolProp.PropsSI('S','T',T[i],'P',P[i],fluidname)
+    # c[i] =  CP.CoolProp.PropsSI('SPEED_OF_SOUND','T',T[i],'P',P[i],fluidname)
+    # u = c[i]*data.iloc[i,1] 
+    # ht[i] = h + 0.5*u*u
+    
+# append new columns
+# shG =pd.DataFrame({'G':G, })
+shG =pd.DataFrame({'G':G, 'Z':Z, 's':s,})
+newData = pd.concat([data, shG], join = 'outer', axis = 1)
+# save newData in csv file
+# newData.to_csv("m4sh.csv")
+newData.to_csv("m4new.csv")
---
+++ NICFD/NC/cfd/postpro/pr/plot_compare.py
@@ -0,0 +1,143 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 16 19:01:08 2022
+
+@author: yan
+"""
+
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 12 13:35:29 2022
+
+@author: yan
+
+"""
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib.ticker import FormatStrFormatter
+import os
+import CoolProp as CP
+from IPython import get_ipython;   
+get_ipython().magic('reset -sf')
+os.system('clear')
+
+
+fluidname = "HEOS::MD4M"
+# fluidname = "HEOS::D6"
+Pc = CP.CoolProp.PropsSI('Pcrit',fluidname)
+Tc = CP.CoolProp.PropsSI('Tcrit',fluidname)
+dc = CP.CoolProp.PropsSI('rhocrit',fluidname)
+
+# mesh0 = pd.read_csv("m1new.csv", ",", skiprows=0)
+# mesh1 = pd.read_csv("m1new.csv", ",", skiprows=0)
+# mesh2 = pd.read_csv("m2new.csv", ",", skiprows=0)
+# mesh3 = pd.read_csv("m3new.csv", ",", skiprows=0)
+mesh4 = pd.read_csv("m4new.csv", ",", skiprows=0)
+
+
+
+# fig 1
+fig1 = plt.figure( dpi=300)
+lwh = 2
+axes = fig1.add_axes([0.15, 0.15, 0.7, 0.7]) #size of figure
+# axes.plot(mesh0.iloc[:,-6] , mesh0.iloc[:,7]/Pc, 'k', lw=lwh, label="5k")
+# axes.plot(mesh2.iloc[:,-6] , mesh2.iloc[:,7]/Pc, 'r', lw=lwh, label="12k")
+# axes.plot(mesh3.iloc[:,-6] , mesh3.iloc[:,7]/Pc, 'g', lw=lwh, label="18k")
+axes.plot(mesh4.iloc[:,-6] , mesh4.iloc[:,7]/Pc, 'b', lw=lwh, label="40k")
+axes.yaxis.set_major_formatter(FormatStrFormatter('%.2f'))
+# sub_axes = plt.axes([0.26, 0.26, 0.25, 0.25]) 
+
+# # sub_axes.plot(m6new.iloc[:,6] , m6new.iloc[:,-3], 'k', lw=lwh, label="$s$")
+# sub_axes.plot(m4new.iloc[:,11] , m4new.iloc[:,-3], 'k', lw=lwh, label="$s$")
+# sub_axes.set_ylabel('$s[J/K/mol]$',fontsize=10) 
+# sub_axes.set_ylim([754.5, 760])
+
+axes.set_xlabel('$X[mm]$',fontsize=12)
+axes.set_ylabel('$P/P_c$',fontsize=12) 
+axes.set_title('$P/P_c$ along y$=0.25 mm$',fontsize=14)
+axes.legend(loc=1) # 
+
+fig1.savefig("shock_gv_p.pdf")
+
+# fig 2
+fig2 = plt.figure( dpi=300)
+lwh = 2
+axes = fig2.add_axes([0.15, 0.15, 0.7, 0.7]) #size of figure
+# axes.plot(mesh0.iloc[:,-6] , mesh0.iloc[:,3], 'k', lw=lwh, label="5k")
+# axes.plot(mesh2.iloc[:,-6] , mesh2.iloc[:,3], 'r', lw=lwh, label="12k")
+# axes.plot(mesh3.iloc[:,-6] , mesh3.iloc[:,3], 'g', lw=lwh, label="18k")
+axes.plot(mesh4.iloc[:,-6] , mesh4.iloc[:,3], 'b', lw=lwh, label="40k")
+axes.yaxis.set_major_formatter(FormatStrFormatter('%.2f'))
+
+
+axes.set_xlabel('$X[mm]$',fontsize=12)
+axes.set_ylabel('Mach',fontsize=12) 
+axes.set_title('Mach number along y$=0.25 mm$',fontsize=14)
+axes.legend(loc=1) # 
+
+fig2.savefig("shock_gv_m.pdf")
+
+# fig 3
+fig3 = plt.figure( dpi=300)
+lwh = 2
+axes = fig3.add_axes([0.15, 0.15, 0.7, 0.7]) #size of figure
+# axes.plot(mesh0.iloc[:,-6] , mesh0.iloc[:,1]/dc, 'k', lw=lwh, label="5k")
+# axes.plot(mesh2.iloc[:,-6] , mesh2.iloc[:,1]/dc, 'r', lw=lwh, label="12k")
+# axes.plot(mesh3.iloc[:,-6] , mesh3.iloc[:,1]/dc, 'g', lw=lwh, label="18k")
+axes.plot(mesh4.iloc[:,-6] , mesh4.iloc[:,1]/dc, 'b', lw=lwh, label="40k")
+
+
+axes.set_xlabel('$X[mm]$',fontsize=12)
+axes.set_ylabel('$\\rho$/$\\rho_c$',fontsize=12) 
+axes.set_title('$\\rho$ / $\\rho_c$ along y$=0.25 mm$',fontsize=14)
+axes.legend(loc=1) # 
+
+fig3.savefig("shock_gv_rho.pdf")
+
+# fig 4
+fig4 = plt.figure( dpi=300)
+lwh = 2
+axes = fig4.add_axes([0.15, 0.15, 0.7, 0.7]) #size of figure
+# axes.plot(mesh0.iloc[:,-6] , mesh0.iloc[:,-3], 'k', lw=lwh, label="5k")
+# axes.plot(mesh2.iloc[:,-6] , mesh2.iloc[:,-3], 'r', lw=lwh, label="12k")
+# axes.plot(mesh3.iloc[:,-6] , mesh3.iloc[:,-3], 'g', lw=lwh, label="18k")
+axes.plot(mesh4.iloc[:,-6] , mesh4.iloc[:,-3], 'b', lw=lwh, label="40k")
+
+# sub_axes = plt.axes([0.55, 0.25, 0.25, 0.25]) 
+# x = []
+# plot the zoomed portion
+# for i in mesh4.iloc[:,-3].index:
+#     if (mesh4.iloc[i,-3])<0:
+#         x.append(i)
+# sub_axes.plot(mesh4.iloc[x[0]:x[-1],-6], mesh4.iloc[x[0]:x[-1],-3] , 'b') 
+# sub_axes.set_ylim([-0.007, -0.002])
+# sub_axes.set_xlim([0, 0.4])
+# sub_axes.yaxis.set_major_formatter(FormatStrFormatter('%.3f'))
+
+axes.set_xlabel('$X[mm]$',fontsize=12)
+axes.set_ylabel('$\\Gamma$',fontsize=12) 
+axes.set_title('$\\Gamma$ along y$=0.25 mm$',fontsize=14)
+axes.legend(loc=0) # 
+
+fig4.savefig("shock_gv_Gamma.pdf")
+
+# fig 5
+fig5 = plt.figure( dpi=300)
+lwh = 2
+axes = fig5.add_axes([0.15, 0.15, 0.7, 0.7]) #size of figure
+# axes.plot(mesh0.iloc[:,-6] , mesh0.iloc[:,-1], 'k', lw=lwh, label="5k")
+# axes.plot(mesh2.iloc[:,-6] , mesh2.iloc[:,-1], 'r', lw=lwh, label="12k")
+# axes.plot(mesh3.iloc[:,-6] , mesh3.iloc[:,-1], 'g', lw=lwh, label="18k")
+axes.plot(mesh4.iloc[:,-6] , mesh4.iloc[:,-1], 'b', lw=lwh, label="40k")
+
+
+axes.yaxis.set_major_formatter(FormatStrFormatter('%.2f'))
+axes.set_xlabel('$X[mm]$',fontsize=12)
+axes.set_ylabel('s',fontsize=12) 
+axes.set_title('entropy along y$=0.25 mm$',fontsize=14)
+axes.legend(loc=1) # 
+
+fig5.savefig("shock_gv_s.pdf")
